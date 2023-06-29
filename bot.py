9535bf360dcbf50f34cab886ab832ae18bb910c0
adityarise2 <137521434+adityarise2@users.noreply.github.com>
2023-06-29 09:44:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ class Bot(Client):
 
         await set_commands(self)
 
-        await broadcast_admins(self, "** Bot start Ayyindhi Ra Babu **")
+        await broadcast_admins(self, "** Bot start Ayyindi mawa **")
         logging.info("Bot started")
 
         if WEB_SERVER:
